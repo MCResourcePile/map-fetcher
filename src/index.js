9b91ade8f7491bd0a15d6438b5052c8f6a0ddbbd
Mitchell Thomas <6386297+mitchts@users.noreply.github.com>
2022-12-14 22:31:25 +1100

    Fix license type being checked even after resolution

--- src/index.js
+++ src/index.js
@@ -214,6 +214,7 @@ const determineMapLicense = async (target, source) => {
       license = licenseTypes[i].license;
       break;
     }
+    if (license !== "unresolved") break;
   }
 
   return license;
